f29bf1cbe2b2ab55e6b6a5f6c64e0bececd2058d
Nate Olson <nathandavidolson@gmail.com>
2015-03-05 09:27:07 -0500

    package template, initial load functions

---
+++ R/present_data.R
@@ -0,0 +1,20 @@
+## Code to generate figures and tables for ROA
+#### Summary of sequencing datasets ####
+# table with number of reads, read length, coverage, ect.
+
+#### Purity ####
+# table of pilon results
+
+##### Base level analysis ####
+## purity plots - script to generate purity plots
+
+## Analysis of low purity positions
+# figures correlating different parameters with low purity positions - e.g. bias and coverage
+
+# table of low purity positions
+
+## indel analysis
+# correlate with homopolymer annotation results
+
+#### Homogeneity ####
+# table, figure, depending on results
