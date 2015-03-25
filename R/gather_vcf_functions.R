0042d27fe1703684821aa9306ccc5f50e5dbb1d3
nathandavidolson@gmail.com <nolson@nist.gov>
2015-03-25 16:13:06 -0400

    changed vcf_db to db_con for consistency

--- R/gather_vcf_functions.R
+++ R/gather_vcf_functions.R
@@ -1,22 +1,22 @@
 # ## Functions for loading data into sqlite db
-.cbtsv_tosql <- function (tsv_file,db_conn,tbl_name) {
+.cbtsv_tosql <- function (tsv_file,db_con,tbl_name) {
   # tsv_file generated using vcf2tsv in vcflib
   # db_conn dplyr sqlite db connection
   #dplyr::tbl_name of table creating in sqlite db
   vcf <- data.table::fread(tsv_file,sep = "\t",header = TRUE,stringsAsFactors = FALSE)
   vcf <- dplyr::rename(vcf, PLATDP=DP) # added to fix issue with two DP columns
-  dplyr::copy_to(vcf_db, vcf, name =dplyr::tbl_name, temporary = FALSE,
+  dplyr::copy_to(db_con, vcf, name =dplyr::tbl_name, temporary = FALSE,
                  indexes = list("CHROM","POS","SAMPLE"))
 }
 
 # Calculating purity and filtering indels
-.pur_tbl <- function (vcf_tbl, db_conn,tbl_name) {
+.pur_tbl <- function (vcf_tbl, db_con,tbl_name) {
   # vcf_tbl sqlite table generated from a vcf file using cbtsv, note requires DP4 info for each sample
   # db_conn dplyr sqlite db connection
   #dplyr::tbl_name of table creating in sqlite db
 
   # dplyr::select desired columns and filtering indels
-  vcf_dp4 <-dplyr::tbl(src = vcf_db, from = vcf_tbl)  %>%
+  vcf_dp4 <-dplyr::tbl(src = db_con, from = vcf_tbl)  %>%
     dplyr::select(CHROM, POS, SAMPLE, INDEL, DP, DP4, SP) %>%
     filter(INDEL==0) %>% dplyr::collect()
 
@@ -29,12 +29,12 @@
            Ref=Ref_For + Ref_Rev,
            Alt = Alt_For + Alt_Rev,
            Pur = Ref/(Ref+ Alt))
-  dplyr::copy_to(db_conn, vcf_dp4, name=tbl_name,temporary = FALSE, indexes = list("CHROM","POS","SAMPLE"))
+  dplyr::copy_to(db_con, vcf_dp4, name=tbl_name,temporary = FALSE, indexes = list("CHROM","POS","SAMPLE"))
 }
 
 # Generating purity by platform summary
-.pur_plat <-function(pur_plat_tbl, db_conn,tbl_name){
- dplyr::tbl(src = vcf_db, from=pur_plat_tbl) %>%
+.pur_plat <-function(pur_plat_tbl, db_con,tbl_name){
+ dplyr::tbl(src = db_con, from=pur_plat_tbl) %>%
     dplyr::group_by(CHROM, POS) %>%
     dplyr::summarize(Ref = sum(Ref), Alt = sum(Alt)) %>%
     dplyr::mutate(Pur = Ref/(Ref + Alt)) %>%
@@ -42,13 +42,13 @@
 }
 
 # Joining two purity platform tables
-.pur_plat_join <- function(pur_plat_tbl1, pur_plat_tbl2, vcf_db,tbl_name,
+.pur_plat_join <- function(pur_plat_tbl1, pur_plat_tbl2, db_con,tbl_name,
                            plat1_name = "plat1", plat2_name = "plat2"){
-  plat1_tbl <-dplyr::tbl(src = vcf_db, from = pur_plat_tbl1) %>%
+  plat1_tbl <-dplyr::tbl(src = db_con, from = pur_plat_tbl1) %>%
     dplyr::group_by(CHROM,POS) %>%
     dplyr::rename(plat1=Pur) %>%
     dplyr::select(CHROM, POS, plat1)
-  plat2_tbl <-dplyr::tbl(src = vcf_db, from = pur_plat_tbl2) %>%
+  plat2_tbl <-dplyr::tbl(src = db_con, from = pur_plat_tbl2) %>%
     dplyr::group_by(CHROM,POS) %>%
     dplyr::rename(plat2=Pur) %>%
     dplyr::select(CHROM, POS, plat2)
