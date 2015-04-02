1d3fcdcdd66c81bb47636c5512d41cc283568ba2
Nate Olson <nathandavidolson@gmail.com>
2015-04-02 16:51:54 -0400

    changes consensus_dir path

--- inst/rmarkdown/templates/pepr-roa/skeleton/make_peprDB.R
+++ inst/rmarkdown/templates/pepr-roa/skeleton/make_peprDB.R
@@ -5,7 +5,7 @@ db_path <- "~/Desktop/micro_rm/pepr-data/MG001/peprDB.sqlite"
 param_yaml <- "~/Desktop/micro_rm/pepr/RM8375_pipeline_reorder_rc_pilon.yaml"
 qc_stats_dir <- "~/Desktop/micro_rm/pepr-data/MG001/Re.Order.RC.CFSAN008157.HGAP_miseq_qc_stats/"
 homogeneity_dir <- "~/Desktop/micro_rm/pepr-data/MG001/Re.Order.RC.CFSAN008157.HGAP_homogeneity/"
-consensus_dir <- "~/Desktop/micro_rm/pepr-data/MG001/Re.Order.RC.CFSAN008157.HGAP_consensus_base/"
+consensus_dir <- "~/Desktop/micro_rm/pepr-data/MG001/Re.Order.RC.CFSAN008157.HGAP_miseq_consensus_base/"
 pilon_dir <- "~/Desktop/micro_rm/pepr-data/MG001/Re.Order.RC.CFSAN008157.HGAP_pilon/"
 purity_dir <- "~/Desktop/micro_rm/pepr-data/MG001/genomic_purity/"
 
