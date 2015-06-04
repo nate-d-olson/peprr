49ea6f6855ba1a852209b22fbb7e666f419914ec
Nate Olson <nathandavidolson@gmail.com>
2015-06-04 18:40:56 -0400

    revised code to remove novel_pilon ref seqs, modified metrics figure

--- R/present_consensus.R
+++ R/present_consensus.R
@@ -1,20 +1,24 @@
 ## code for generating base level purity results
-# db_con <- dplyr::src_sqlite(db_path)
-# library(dplyr)
-# library(ggplot)
 # cb tables  - raw vcf input
 # pur tables - per sample, position purity summary - includes forward and reverse ref and alt counts
 # pur_miseq_pooled - position purity summary by platform
 # pur_join - pairwise table for purity scatter plot
 
-## need to rerun make peprDB - wtih new data
-## excluding novel seqeunces added by pilon
+## list of non-pilon novel chromosomes
+.get_chrom_names <- function(db_con){
+    dplyr::tbl(db_con, "pur_join") %>%
+        dplyr::select(CHROM) %>%
+        dplyr::collect() %>%
+        .$CHROM %>% unique() %>%
+        grep("novel", ., invert = TRUE, value = TRUE)
+}
 
 purity_scatter_plot <- function (db_con, plat1_name = "Miseq", plat2_name = "PGM") {
     dplyr::tbl(db_con, "pur_join") %>%
-        dplyr::filter(plat1 < 0.98 | plat2 < 0.98, CHROM != "pilon_novel_001" ) %>%
+        dplyr::filter(plat1 < 0.98 | plat2 < 0.98,
+                      CHROM %in% .get_chrom_names(db_con)) %>%
         dplyr::collect() %>%
-        dplyr::filter(CHROM %in% grep("novel", CHROM,value = TRUE,invert = TRUE)) %>%
+        dplyr::filter(CHROM %in% grep("novel", CHROM, value = TRUE, invert = TRUE)) %>%
         ggplot2::ggplot() +
             ggplot2::geom_point(ggplot2::aes(x = plat1, y = plat2),
                                 alpha = 0.5) +
@@ -23,51 +27,66 @@ purity_scatter_plot <- function (db_con, plat1_name = "Miseq", plat2_name = "PGM
 
 low_purity_table <- function(db_con){
     low_pur <- dplyr::tbl(db_con, "pur_join") %>%
-        dplyr::filter(plat1 < 0.98, plat2 < 0.98, CHROM != "pilon_novel_001" ) %>% dplyr::collect() %>%
+        dplyr::filter(plat1 < 0.98 | plat2 < 0.98,
+                      CHROM %in% .get_chrom_names(db_con)) %>%
+        dplyr::collect() %>%
         dplyr::select(-plat1, -plat2)
+
     low_miseq <- dplyr::tbl(db_con, "pur_miseq_pooled") %>%
                     dplyr::filter(CHROM %in% low_pur$CHROM,
                                   POS %in% low_pur$POS) %>%
                     dplyr::collect()  %>%
                     dplyr::right_join(low_pur)
+
     low_pgm <- dplyr::tbl(db_con, "pur_pgm_pooled") %>%
         dplyr::filter(CHROM %in% low_pur$CHROM,
                       POS %in% low_pur$POS) %>%
         dplyr::collect()  %>%
         dplyr::right_join(low_pur)
+
     low_join <- dplyr::left_join(low_miseq, low_pgm, by = c("CHROM", "POS"))
+
     names(low_join) <- names(low_join) %>%
         stringr::str_replace_all("x","miseq") %>%
         stringr::str_replace_all("y","pgm")
     low_join
 }
 
-get_low_pur_metrics <- function(df, low_pur){
+.get_low_pur_metrics <- function(df, low_pur){
     df  %>% dplyr::filter(CHROM %in% low_pur$CHROM,
                   POS %in% low_pur$POS) %>%
         dplyr::collect()  %>%
         dplyr::right_join(low_pur) %>%
         dplyr::select(CHROM, POS, RPB, MQB, BQB, MQSB, MQ0F) %>%
         dplyr::group_by(CHROM, POS) %>%
-        dplyr::summarise_each(funs(mean))
+        dplyr::summarise_each(dplyr::funs(mean))
 }
+
 low_pur_metrics <- function(db_con){
+    chrom_names <- .get_chrom_names(db_con)
     low_pur <- dplyr::tbl(db_con, "pur_join") %>%
-        dplyr::filter(plat1 < 0.98, plat2 < 0.98) %>% dplyr::collect() %>%
+        dplyr::filter(plat1 < 0.98, plat2 < 0.98,
+            CHROM %in% chrom_names) %>%
+        dplyr::collect() %>%
         dplyr::select(-plat1, -plat2)
     low_miseq <- dplyr::tbl(db_con, "cb_miseq") %>%
-        get_low_pur_metrics(low_pur) %>%
+        .get_low_pur_metrics(low_pur) %>%
         dplyr::mutate(plat = "miseq")
-    low_pgm <- dplyr::tbl(db_con, "cb_pgm") %>% get_low_pur_metrics(low_pur) %>%
+    low_pgm <- dplyr::tbl(db_con, "cb_pgm") %>%
+        .get_low_pur_metrics(low_pur) %>%
         dplyr::mutate(plat = "pgm")
     dplyr::bind_rows(low_miseq, low_pgm)
 }
 
 low_pur_metrics_figure <- function(db_con){
     low_pur_metrics(db_con) %>% tidyr::gather("metric","value", 3:7) %>%
-        ggplot2::ggplot() + ggplot2::geom_point(ggplot2::aes(x = metric, y = value, color = plat)) +
-            ggplot2::facet_wrap(~POS) + ggplot2::theme_bw() +
-            ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90))
+        ggplot2::ggplot() +
+            ggplot2::geom_point(ggplot2::aes(x = as.character(POS),
+                                             y = value, color = plat)) +
+            ggplot2::facet_wrap(~metric, ncol = 1) + ggplot2::theme_bw() +
+            ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90),
+                           legend.position = "bottom", legend.direction = "horizontal") +
+            ggplot2::labs(x = "Position", color = "Platform")
 }
 
 
