96678f96cd722a895bf90a67de13d0ea1c968fab
Nate Olson <nathandavidolson@gmail.com>
2015-06-05 12:16:56 -0400

    re-org presentation functions

--- R/present_contamination.R
+++ R/present_contamination.R
@@ -1,71 +1,9 @@
-## Code to generate figures and tables for ROA
-#### Summary of sequencing datasets --------------------------------------------
-#' create df for dataset summary table
-#' @param db_con peprDB connection
-#' @return NULL
-seq_summary_table <- function(db_con){
-    # table with number of reads, read length, coverage, ect.
-    seq_metrics <- dplyr::tbl(src = db_con, from="align")  %>%
-        dplyr::select(accession, CATEGORY, TOTAL_READS, MEAN_READ_LENGTH)  %>%
-        dplyr::filter(CATEGORY %in% c("UNPAIRED","PAIR"))  %>%
-        dplyr::collect()  %>%
-        tidyr::separate(accession, c("ref","pilon", "accession"), sep = "_")
-
-    insert_tbl <- dplyr::tbl(src = db_con, from="insert_hist")  %>%
-        dplyr::group_by(accession)  %>%
-        dplyr::summarize(mean_insert =
-            sum(insert_size *All_Reads.fr_count)/sum(All_Reads.fr_count))  %>%
-        dplyr::collect()  %>%
-        tidyr::separate(accession, c("ref","pilon", "accession"), sep = "_")
-
-    coverage_tbl <- dplyr::tbl(src = db_con, from="coverage") %>%
-        dplyr::rename("accession"=SAMPLE) %>%
-        dplyr::collect()
-
-    dplyr::tbl(src = db_con, from ="exp_design")  %>%
-        dplyr::collect() %>% dplyr::full_join(seq_metrics) %>%
-        dplyr::full_join(insert_tbl) %>%
-        dplyr::full_join(coverage_tbl) %>%
-        dplyr::select(-ref, -pilon,-CATEGORY) %>%
-        dplyr::rename("Accession" = accession, "Platform" = plat,
-                      "Vial" = vial, "Replicate" = rep,
-                      "Reads"= TOTAL_READS, "Read Length" = MEAN_READ_LENGTH,
-                      "Insert Size"=mean_insert, "Coverage"= COV)
-}
-
-#### Pilon Changes -------------------------------------------------------------
-#' create df for pilon changes
-#' @param db_con peprDB connection
-#' @return NULL
-pilon_changes_table <- function(db_con){
-    dplyr::tbl(src = db_con, from="pilon_changes")  %>%
-        dplyr::collect()
-}
-
-##### Consensus base -----------------------------------------------------------
-## purity plots - script to generate purity plots
-
-
-## Analysis of low purity positions
-# figures correlating different parameters with low purity positions
-# e.g. bias and coverage
-
-# table of low purity positions
-
-## indel analysis
-# correlate with homopolymer annotation results
-
-### Homogeneity ----------------------------------------------------------------
-# table and figures in present_homogeneity.R
-
-
-
 ### Genomic Purity -------------------------------------------------------------
 .genomic_purity_df <- function(db_con,genus, db_ref_root = "micro_rm_patho_db_"){
     purity_df <- dplyr::tbl(src = db_con, from = "purity") %>%
         dplyr::collect() %>%
         dplyr::mutate(accession = stringr::str_replace(pattern = db_ref_root,
-                                       replacement = "",accession))
+                                                       replacement = "",accession))
 
     purity_df <- dplyr::tbl(src = db_con, from = "exp_design") %>%
         dplyr::collect() %>%
@@ -83,13 +21,13 @@ pilon_changes_table <- function(db_con){
 #' @return NULL
 contam_counts_figure <- function(db_con, genus){
     df <- .genomic_purity_df(db_con, genus) %>%
-                dplyr::filter(Contam == FALSE) %>%
-                dplyr::group_by(accession, plat, vial) %>%
-                dplyr::summarize(total_prop = sum(Final.Guess),
-                                prop_read = 1000000 *(1-total_prop))
+        dplyr::filter(Contam == FALSE) %>%
+        dplyr::group_by(accession, plat, vial) %>%
+        dplyr::summarize(total_prop = sum(Final.Guess),
+                         prop_read = 1000000 *(1-total_prop))
     ggplot2::ggplot(df) + ggplot2::geom_boxplot(ggplot2::aes(x = plat,
-                                           y = prop_read,
-                                           color = plat)) +
+                                                             y = prop_read,
+                                                             color = plat)) +
         ggplot2::theme_bw() +
         ggplot2::labs(x = "Sequencing Platform",y = "Contaminants/Million Reads") +
         ggplot2::theme(legend.position = "none")
@@ -103,14 +41,14 @@ contam_distribution_figure <- function(db_con,genus){
     .genomic_purity_df(db_con, genus) %>%
         dplyr::filter(Contam == TRUE) %>%
         ggplot2::ggplot() +
-            ggplot2::geom_density(ggplot2::aes(x = Final.Best.Hit.Read.Numbers,
-                                               fill = plat),
-                                                alpha = 0.5) +
-            ggplot2::scale_x_log10() + ggplot2::theme_bw() +
-            ggplot2::labs(x = "Number of Reads",y = "Density",
-                          fill= "Platform") +
-            ggplot2::theme(legend.position = c(0.65,0.9),
-                           legend.direction = "horizontal")
+        ggplot2::geom_density(ggplot2::aes(x = Final.Best.Hit.Read.Numbers,
+                                           fill = plat),
+                              alpha = 0.5) +
+        ggplot2::scale_x_log10() + ggplot2::theme_bw() +
+        ggplot2::labs(x = "Number of Reads",y = "Density",
+                      fill= "Platform") +
+        ggplot2::theme(legend.position = c(0.65,0.9),
+                       legend.direction = "horizontal")
 }
 
 #' create contaminant heatmap
---
+++ R/present_seq_data.R
@@ -0,0 +1,44 @@
+## Code to generate figures and tables for ROA
+#### Summary of sequencing datasets --------------------------------------------
+#' create df for dataset summary table
+#' @param db_con peprDB connection
+#' @return NULL
+seq_summary_table <- function(db_con){
+    # table with number of reads, read length, coverage, ect.
+    seq_metrics <- dplyr::tbl(src = db_con, from="align")  %>%
+        dplyr::select(accession, CATEGORY, TOTAL_READS, MEAN_READ_LENGTH)  %>%
+        dplyr::filter(CATEGORY %in% c("UNPAIRED","PAIR"))  %>%
+        dplyr::collect()  %>%
+        tidyr::separate(accession, c("ref","pilon", "accession"), sep = "_")
+
+    insert_tbl <- dplyr::tbl(src = db_con, from="insert_hist")  %>%
+        dplyr::group_by(accession)  %>%
+        dplyr::summarize(mean_insert =
+            sum(insert_size *All_Reads.fr_count)/sum(All_Reads.fr_count))  %>%
+        dplyr::collect()  %>%
+        tidyr::separate(accession, c("ref","pilon", "accession"), sep = "_")
+
+    coverage_tbl <- dplyr::tbl(src = db_con, from="coverage") %>%
+        dplyr::rename("accession"=SAMPLE) %>%
+        dplyr::collect()
+
+    dplyr::tbl(src = db_con, from ="exp_design")  %>%
+        dplyr::collect() %>% dplyr::full_join(seq_metrics) %>%
+        dplyr::full_join(insert_tbl) %>%
+        dplyr::full_join(coverage_tbl) %>%
+        dplyr::select(-ref, -pilon,-CATEGORY) %>%
+        dplyr::rename("Accession" = accession, "Platform" = plat,
+                      "Vial" = vial, "Replicate" = rep,
+                      "Reads"= TOTAL_READS, "Read Length" = MEAN_READ_LENGTH,
+                      "Insert Size"=mean_insert, "Coverage"= COV)
+}
+
+#### Pilon Changes -------------------------------------------------------------
+#' create df for pilon changes
+#' @param db_con peprDB connection
+#' @return NULL
+pilon_changes_table <- function(db_con){
+    dplyr::tbl(src = db_con, from="pilon_changes")  %>%
+        dplyr::collect()
+}
+
