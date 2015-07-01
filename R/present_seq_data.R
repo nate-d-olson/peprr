0d19e0adab9542f43ded51077fb421dc3db96367
Nate Olson <nathandavidolson@gmail.com>
2015-07-01 19:54:32 -0400

    modified to work with all sample names as long as accession is after the last "_"

--- R/present_seq_data.R
+++ R/present_seq_data.R
@@ -8,15 +8,20 @@ seq_summary_table <- function(db_con){
     seq_metrics <- dplyr::tbl(src = db_con, from="align")  %>%
         dplyr::select(accession, CATEGORY, TOTAL_READS, MEAN_READ_LENGTH)  %>%
         dplyr::filter(CATEGORY %in% c("UNPAIRED","PAIR"))  %>%
-        dplyr::collect()  %>%
-        tidyr::separate(accession, c("ref","pilon", "accession"), sep = "_")
+        dplyr::collect() %>%
+        dplyr::mutate(accession = gsub(pattern = ".+_",
+                                       replacement = "",
+                                       x = accession))
+
 
     insert_tbl <- dplyr::tbl(src = db_con, from="insert_hist")  %>%
         dplyr::group_by(accession)  %>%
         dplyr::summarize(mean_insert =
             sum(insert_size *All_Reads.fr_count)/sum(All_Reads.fr_count))  %>%
         dplyr::collect()  %>%
-        tidyr::separate(accession, c("ref","pilon", "accession"), sep = "_")
+        dplyr::mutate(accession = gsub(pattern = ".+_",
+                                       replacement = "",
+                                       x = accession))
 
     coverage_tbl <- dplyr::tbl(src = db_con, from="coverage") %>%
         dplyr::rename("accession"=SAMPLE) %>%
@@ -26,7 +31,7 @@ seq_summary_table <- function(db_con){
         dplyr::collect() %>% dplyr::full_join(seq_metrics) %>%
         dplyr::full_join(insert_tbl) %>%
         dplyr::full_join(coverage_tbl) %>%
-        dplyr::select(-ref, -pilon,-CATEGORY) %>%
+        dplyr::select(-CATEGORY) %>%
         dplyr::rename("Accession" = accession, "Platform" = plat,
                       "Vial" = vial, "Replicate" = rep,
                       "Reads"= TOTAL_READS, "Read Length" = MEAN_READ_LENGTH,
